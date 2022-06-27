20d88fdc723e6b058a93da3382adad9e3671acd8
nelsoneo <nelsoneo@gmail.com>
2022-06-27 15:07:40 -0300

    init

---
+++ starter/js/app.js
@@ -0,0 +1,22 @@
+
+/* function for get tag element html */
+const getElement = (selector) => {
+  const element = document.querySelector(selector);
+  if (element) return element;
+  throw Error(
+    `Please double check your class names, there is no ${selector} class`
+  );
+};
+
+const links = getElement(".nav-links");
+const navBtnDOM = getElement(".nav-btn");
+
+/* add class at button for open and closed menu */
+navBtnDOM.addEventListener("click", () => {
+  links.classList.toggle("show-links");
+});
+
+/* current date on the footer page */
+const date = getElement("#date");
+const currentYear = new Date().getFullYear();
+date.textContent = currentYear;
